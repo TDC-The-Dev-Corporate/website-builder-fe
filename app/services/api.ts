31342229a33c08e271620559410cbb2192a27b23
Mahroosh <mahroosh16hashmi@gmail.com>
2025-03-13 12:21:29 +0500

    cloudnary integration for image handling

--- app/services/api.ts
+++ app/services/api.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosResponse } from "axios";
 const api = axios.create({
-  // baseURL: "https://97de-116-58-9-130.ngrok-free.app",
-  baseURL: "http://localhost:3001/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     "ngrok-skip-browser-warning": true,
   },
