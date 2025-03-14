e65b99c28a41d7a000075084f816a7478d59676a
Mahroosh <mahroosh16hashmi@gmail.com>
2025-03-14 15:33:02 +0500

    landing page and auth pages

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+import {
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+  CircularProgress,
+  Alert,
+} from "@mui/material";
+
+import { AppDispatch, RootState } from "@/lib/redux/store";
+import { login } from "@/lib/redux/slices/authSlice";
+
+export default function Login() {
+  const dispatch = useDispatch<AppDispatch>();
+  const router = useRouter();
+  const { loading, error } = useSelector((state: RootState) => state.auth);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data: any) => {
+    const result = await dispatch(login(data));
+    if (!result.payload.error) {
+      router.push("/dashboard");
+    }
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        {error && <Alert severity="error">{error}</Alert>}
+        <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Email Address"
+            {...register("email", { required: true })}
+            error={!!errors.email}
+            helperText={errors.email ? "Email is required" : ""}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Password"
+            type="password"
+            {...register("password", { required: true })}
+            error={!!errors.password}
+            helperText={errors.password ? "Password is required" : ""}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            disabled={loading}
+          >
+            {loading ? <CircularProgress size={24} /> : "Sign In"}
+          </Button>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Link href="/auth/register">Don't have an account? Sign Up</Link>
+            <Link href="/auth/forgot-password">Forgot password?</Link>
+          </Box>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
---
+++ lib/redux/api/auth.ts
@@ -0,0 +1,21 @@
+import api from "@/app/services/api";
+
+export const registerUser = async (data: any) => {
+  const response = await api.post("auth/register", data);
+  return response.data;
+};
+
+export const verify = async (data: any) => {
+  const response = await api.post("auth/verifyUser", data);
+  return response.data;
+};
+
+export const loginUser = async (data: any) => {
+  const response = await api.post("auth/login", data);
+  return response.data;
+};
+
+export const sendOtp = async (data: any) => {
+  const response = await api.post("auth/sendOTP", data);
+  return response.data;
+};
