4a3df62df7fd9e420db614d4227b37b9ae34a5a6
Mahroosh <mahroosh16hashmi@gmail.com>
2025-03-18 16:33:52 +0500

    add google login

---
+++ app/AIWebsiteBuilders/oauth-redirect/page.tsx
@@ -0,0 +1,22 @@
+// pages/oauth-redirect.tsx
+"use client";
+import { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
+
+const OAuthRedirect = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = new URLSearchParams(pathname.split("?")[1]);
+  const token = searchParams.get("token");
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("access_token", token as string);
+      router.push("/AIWebsiteBuilders/template-selector");
+    }
+  }, [token, router]);
+
+  return <div>Loading...</div>;
+};
+
+export default OAuthRedirect;
