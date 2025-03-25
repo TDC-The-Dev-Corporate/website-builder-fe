a6ec5a1d5b0d5b55cc275dbeefd4a857eda83155
Mahroosh <mahroosh16hashmi@gmail.com>
2025-03-25 10:05:23 +0500

    display dynamic fonts

--- lib/utils.ts
+++ lib/utils.ts
@@ -21,3 +21,18 @@ export function getUserId(token: string): number | null {
     return null;
   }
 }
+
+export const getFontSize = (size: string) => {
+  const sizes = {
+    xs: "0.75rem",
+    sm: "0.875rem",
+    base: "1rem",
+    lg: "1.125rem",
+    xl: "1.25rem",
+    "2xl": "1.5rem",
+    "3xl": "1.875rem",
+    "4xl": "2.25rem",
+    "5xl": "3rem",
+  };
+  return sizes[size] || "1rem";
+};
