aa59a4ec0aa1259c291aab266abe5548f6126057
Mahroosh <mahroosh16hashmi@gmail.com>
2025-04-07 15:54:40 +0500

    check for published website

--- app/AIWebsiteBuilders/template-selector/page.tsx
+++ app/AIWebsiteBuilders/template-selector/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import { useEffect, useState } from "react";
 
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import {
   Container,
   Box,
@@ -16,47 +17,85 @@ import ViewProfile from "../profile/page";
 import ProfileURLCard from "../profile/profileUrl";
 import TemplateSelector from "@/app/components/TemplateSelector";
 
+import { getPortfolioByUserName } from "@/lib/redux/api/portfolio";
+import AppLoader from "@/app/components/loader/AppLoader";
+
 import { logoutUser } from "@/lib/utils";
 
 export default function Home() {
   const username = JSON.parse(localStorage.getItem("user")).username;
 
-  return (
-    <Box sx={{ minHeight: "100vh", bgcolor: "grey.100" }}>
-      <AppBar position="static" elevation={1} color="default">
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            AI Website Builders
-          </Typography>
-          <Button sx={{ color: "#0d47a1" }} onClick={() => logoutUser()}>
-            <LogoutOutlinedIcon />
-          </Button>
-        </Toolbar>
-      </AppBar>
+  const [portfolio, setPortfolio] = useState<Portfolio | null>(null);
+  const [loading, setLoading] = useState(true);
 
-      <Container
-        maxWidth={false}
-        sx={{
-          py: 4,
-          width: "100%",
-          mx: "auto",
-        }}
-      >
-        <Paper elevation={3} sx={{ p: 5, mb: 2 }}>
-          <ViewProfile />
-        </Paper>
-        <Paper elevation={3} sx={{ p: 5, mb: 2 }}>
-          <ProfileURLCard
-            url={`http://localhost:3000/AIWebsiteBuilders/portfolio/${username}`}
-          />
-        </Paper>
-        <Paper elevation={3} sx={{ p: 5 }}>
-          <Typography variant="h5" sx={{ mb: 2 }}>
-            Choose a Template
-          </Typography>
-          <TemplateSelector />
-        </Paper>
-      </Container>
-    </Box>
+  useEffect(() => {
+    async function fetchPortfolio() {
+      if (!username) return;
+
+      try {
+        const response = getPortfolioByUserName(username);
+        if (!response) {
+          localStorage.setItem("published", "false");
+          throw new Error("Failed to fetch portfolio");
+        }
+
+        const data = await response;
+        console.log("response", response);
+        console.log("data", data);
+        setPortfolio(data);
+        setLoading(false);
+        data
+          ? localStorage.setItem("published", "true")
+          : localStorage.setItem("published", "false");
+      } catch (error) {
+        localStorage.setItem("published", "false");
+        console.error("Error fetching portfolio:", error);
+      }
+    }
+
+    fetchPortfolio();
+  }, [username]);
+
+  return (
+    <AppLoader loading={loading}>
+      <Box sx={{ minHeight: "100vh", bgcolor: "grey.100" }}>
+        <AppBar position="static" elevation={1} color="default">
+          <Toolbar>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              AI Website Builders
+            </Typography>
+            <Button sx={{ color: "#0d47a1" }} onClick={() => logoutUser()}>
+              <LogoutOutlinedIcon />
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <Container
+          maxWidth={false}
+          sx={{
+            py: 4,
+            width: "100%",
+            mx: "auto",
+          }}
+        >
+          <Paper elevation={3} sx={{ p: 5, mb: 2 }}>
+            <ViewProfile />
+          </Paper>
+          {portfolio && (
+            <Paper elevation={3} sx={{ p: 5, mb: 2 }}>
+              <ProfileURLCard
+                url={`http://localhost:3000/AIWebsiteBuilders/portfolio/${username}`}
+              />
+            </Paper>
+          )}
+          <Paper elevation={3} sx={{ p: 5 }}>
+            <Typography variant="h5" sx={{ mb: 2 }}>
+              Choose a Template
+            </Typography>
+            <TemplateSelector />
+          </Paper>
+        </Container>
+      </Box>
+    </AppLoader>
   );
 }
