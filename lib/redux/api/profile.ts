d47a857af25ff3ad20ea8cd08d60a30b471e1de9
Mahroosh <mahroosh16hashmi@gmail.com>
2025-04-09 12:29:57 +0500

    delete account module

--- lib/redux/api/profile.ts
+++ lib/redux/api/profile.ts
@@ -4,3 +4,8 @@ export const updateUser = async (data: any) => {
   const response = await api.put("user", data);
   return response.data;
 };
+
+export const removeUser = async () => {
+  const response = await api.delete("user");
+  return response.data;
+};
