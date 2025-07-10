fc9a52e4ba0c92561d7ecaeaa5eb31f9c83ab812
Mahroosh <mahroosh16hashmi@gmail.com>
2025-07-10 10:11:18 +0500

    try to fix tinymce production bug

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["@grapesjs/studio-sdk-plugins"],
+  experimental: {
+    esmExternals: "loose",
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
